3ed2ea820a30d59675a3ff50a6c7192bdb17b683
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-03 00:29:02 +0100

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -55,7 +55,9 @@ class TodoList:
         """
         Export the employee todo tasks to json.
         """
-        data = [{"task": todo.get("title"), "completed": todo.get("completed"), "username": self.name} for todo in self.tasks]
+        data = [{"task": todo.get("title"),
+                 "completed": todo.get("completed"),
+                 "username": self.name} for todo in self.tasks]
         user_data = {"{}".format(self.employee_id): data}
         with open('{}.json'.format(self.employee_id), 'w') as json_file:
             json.dump(user_data, json_file)
