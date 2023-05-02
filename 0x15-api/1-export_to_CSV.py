081ba014c3d711cd8b2ccbb015306f91b18b9518
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-02 23:18:11 +0100

    export to csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+This module retrieves and export data tpo CSV file.
+"""
+import requests
+import sys
+
+
+class TodoList:
+    """
+    This module retrieves and export data tpo CSV file.
+    """
+
+    def __init__(self, employee_id):
+        """
+        Initializes the TodoList object with the given employee ID.
+        """
+        self.employee_id = employee_id
+        self.name = self._get_employee_name()
+        self.tasks = self._get_employee_tasks()
+
+    def _get_employee_name(self):
+        """
+        Retrieves the name of the employee with the initialized ID.
+        """
+        response = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                                .format(self.employee_id))
+        response_dict = response.json()
+        return response_dict['name']
+
+    def _get_employee_tasks(self):
+        """
+        Retrieves the tasks of the employee with the initialized ID.
+        """
+        response = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            self.employee_id))
+        response_list = response.json()
+        return response_list
+
+    def export_to_csv(self):
+        """
+        Displays the employee TODO list progress in the specified format.
+        """
+        with open("USER_ID.csv", "w") as f:
+            for todo in self.tasks:
+                f.writelines("{}, {}, {}, {}".format(
+                    self.id, self.name, todo["correction"], todo["title"]))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: {} employee_id'.format(sys.argv[0]))
+        sys.exit(1)
+
+    employee_id = sys.argv[1]
+    todo_list = TodoList(employee_id)
+    todo_list.export_to_csv()
