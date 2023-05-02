0c6219602417ee21900004aeeaf075f25afde23c
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-02 22:46:54 +0100

    gather data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,57 +1,33 @@
 #!/usr/bin/python3
 """
-This module retrieves and displays a given employee's TODO list progress.
+Using https://jsonplaceholder.typicode.com
+returns info about employee TODO progress
+Implemented using recursion
 """
+import re
 import requests
 import sys
 
 
-class TodoList:
-    """
-    This class retrieves and displays a given employee's TODO list progress.
-    """
-
-    def __init__(self, employee_id):
-        """
-        Initializes the TodoList object with the given employee ID.
-        """
-        self.employee_id = employee_id
-        self.name = self._get_employee_name()
-        self.tasks = self._get_employee_tasks()
-
-    def _get_employee_name(self):
-        """
-        Retrieves the name of the employee with the initialized ID.
-        """
-        response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(self.employee_id))
-        response_dict = response.json()
-        return response_dict['name']
-
-    def _get_employee_tasks(self):
-        """
-        Retrieves the tasks of the employee with the initialized ID.
-        """
-        response = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(self.employee_id))
-        response_list = response.json()
-        return response_list
-
-    def display_progress(self):
-        """
-        Displays the employee TODO list progress in the specified format.
-        """
-        total_tasks = len(self.tasks)
-        done_tasks = sum(1 for task in self.tasks if task['completed'])
-        print('Employee {} is done with tasks({}/{}):'.format(self.name, done_tasks, total_tasks))
-        for task in self.tasks:
-            if task['completed']:
-                print('\t {} {}'.format(task['title'], '(completed)'))
+API = "https://jsonplaceholder.typicode.com"
+"""REST API url"""
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage: {} employee_id'.format(sys.argv[0]))
-        sys.exit(1)
-
-    employee_id = sys.argv[1]
-    todo_list = TodoList(employee_id)
-    todo_list.display_progress()
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            user_res = requests.get('{}/users/{}'.format(API, id)).json()
+            todos_res = requests.get('{}/todos'.format(API)).json()
+            user_name = user_res.get('name')
+            todos = list(filter(lambda x: x.get('userId') == id, todos_res))
+            todos_done = list(filter(lambda x: x.get('completed'), todos))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    user_name,
+                    len(todos_done),
+                    len(todos)
+                )
+            )
+            for todo_done in todos_done:
+                print('\t {}'.format(todo_done.get('title')))
