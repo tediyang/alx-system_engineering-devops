533755420871d841cb688d949c61715a20ec6e2f
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-14 14:11:35 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,16 +11,18 @@ def top_ten(subreddit):
     Returns the top ten posts for a
     given subreddit
     """
-    if subreddit is None and type(subreddit) is not str:
+
+    if not subreddit and type(subreddit) != str:
         return 0
+
     route = 'https://www.reddit.com'
-    header = {'User-Agent': 'tediyang'}
     params = {'limit': 10}
     r = req.get('{}/r/{}/hot.json'.format(route, subreddit), params=params,
-                  headers=header, allow_redirects=False).json()
+                headers={'User-Agent': 'tediyang'},
+                allow_redirects=False)
     if r.status_code == 404:
         print("None")
         return
-    results = r.get("data")
+    results = r.json().get("data")
     [print(c.get("data").get("title")) for c in results.get("children")]
     
