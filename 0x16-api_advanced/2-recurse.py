287c0fa63aecf5ae20cd98a75b5b557ed9ddb563
Eyang, Daniel Eyoh <daniel.eyang.ed@gmail.com>
2023-05-14 14:22:42 +0100

    recurse

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Queries Reddit API to get all hot articles of a subreddit
+"""
+import requests as req
+
+
+def recurse(subreddit, hot_list=[], after=''):
+    """
+    Get all the hot articles of a subreddit
+    """
+    li = []
+    base = 'https://www.reddit.com'
+    header = {'User-agent': 'n_onyekachukwu'}
+    respons = req.get('{}/r/{}/hot.json?after={}&limit=200'
+                      .format(base, subreddit, after),
+                      headers=header, allow_redirects=False)
+    if respons.status_code != 200:
+        return None
+    posts = respons.json().get('data').get('children')
+    _next = respons.json().get('data').get('after')
+    for post in posts:
+        hot_list.append(post.get('data').get('title'))
+
+    if _next:
+        li.append(hot_list)
+        recurse(subreddit, hot_list, _next)
+    return hot_list
